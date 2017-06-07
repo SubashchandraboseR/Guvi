912c54e05f4ae021b617e4fe79c6833bf29b6307
SubashchandraboseR <rsubashbose2015@gmail.com>
2017-06-07 15:34:48 +0530

    Add files via upload

---
+++ Even_Odd.java
@@ -0,0 +1,12 @@
+public class Even_Odd {
+public static void main(String args[]){
+	Scanner sc=new Scanner(System.in);
+	System.out.print("Enter the number:");
+	int num=sc.nextInt();
+	if(num%2==0)
+		System.out.println("Even");
+	else
+		System.out.print("odd");
+	
+}
+}
