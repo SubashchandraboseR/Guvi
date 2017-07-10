74c1f600c1b4fbe61fb94684a5be90015f8a3e45
SubashchandraboseR <rsubashbose2015@gmail.com>
2017-07-10 16:44:55 +0530

    Add files via upload

--- CamelCase.java
+++ CamelCase.java
@@ -1,18 +1,33 @@
-import java.util.*;
-public class Camelcase
-{
-public static void main(String[] args) 
-{
-    Scanner s=new Scanner(System.in);
-    String str=new String();
-    p=s.next();
-    StringBuffer a = new StringBuffer();
-   for (String str:p.split(" ")) {
-    a.append(Character.toUpperCase(s.charAt(0)));
-     if (str.length() > 1) {
-         a.append(str.substring(1, str.length()).toLowerCase());
-      }
-    }
-   System.out.println(a);
-}
+import java.util.Scanner;
+
+public class CamelCase {
+	public static void main(String[] args) {
+		
+	    Scanner input=new Scanner(System.in);
+	    int count=0;
+	    int flag=0;
+	    System.out.print("Enter a string:");
+	    String str=input.next();
+	
+	    for(int i=0;i<str.length();i++)
+	    {
+	    	if(str.charAt(i)>=65&&str.charAt(i)<=90) //Check the UpperCase Letters.
+	    	{
+	    		count++;
+	    	}
+	    	if(str.charAt(i)>=97&&str.charAt(i)<=122) //Check the LowerCase Letters.
+	    	{
+	    		flag=1;
+	    	}
+	    }
+	    
+	    if(flag==1&&count>1)
+	    {
+	    	System.out.println("The Given String is CamelCase");
+	    }
+	    else
+	    {
+	    	System.out.println("The Given String is Not a CamelCase");
+	    }
+	}
 }
