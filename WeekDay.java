8ac5decd583308ad1ec82ac48b4ec69550d56341
SubashchandraboseR <rsubashbose2015@gmail.com>
2017-07-11 10:27:50 +0530

    Update WeekDay.java

--- WeekDay.java
+++ WeekDay.java
@@ -5,18 +5,18 @@ public class WeekDay {
 	public static void main(String[] args) {
 		
 		Scanner s=new Scanner(System.in);
-		System.out.println("Enter a Day: ");
+		System.out.println("Enter a Day in Full Form: ");
 		String a=s.next();
 		
 		a=a.toLowerCase();
 		
-		if(a.equals("mon") || a.equals("tue") || a.equals("wed") || a.equals("thu") 
-				|| a.equals("fri")) {
+		if(a.equals("monday") || a.equals("tuesday") || a.equals("wednesday") || a.equals("thursday") 
+				|| a.equals("friday")) {
 			
 			System.out.println("True\nThe day is WeekDay");
 		}
 		
-		if(a.equals("sat") || a.equals("sun")) {
+		if(a.equals("saturday") || a.equals("sunday")) {
 			
 			System.out.println("False\nThe day is WeekEnd");
 		}
