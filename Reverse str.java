d4d6aa5149c1074eb04ac17a626bcfc4c62e8796
SubashchandraboseR <rsubashbose2015@gmail.com>
2017-07-24 14:24:45 +0530

    Update Reverse str.java

--- Reverse str.java
+++ Reverse str.java
@@ -2,7 +2,7 @@ public class Reversestr {
 	public static void main(String args[]){
     Scanner ch=new Scanner(System.in);
     System.out.print("Enter the String= ");
-	String str=ch.next(); 
+	String str=ch.nextLine(); 
 	StringBuilder st=new StringBuilder(str);
 	StringBuilder str1=st.reverse();
     System.out.print("The reverse String is= "+str1);
